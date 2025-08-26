c85e13974c5fa575f6a6a7fd3cb5a0cc67066075
Linnea <linnea.ternevik@gmail.com>
2025-08-26 12:24:50 +0200

    feat(week1/day5-codewars): add first Codewars kata solution (sum function)

---
+++ week1/day5-codewars/solution1.js
@@ -0,0 +1,10 @@
+// Day 5 – Codewars: Sum of two numbers
+
+function sum(a, b) {
+  return a + b;
+}
+
+// quick test
+console.log(sum(2, 3)); // 5
+console.log(sum(-1, 1)); // 0
+console.log(sum(10, 20)); // 30
